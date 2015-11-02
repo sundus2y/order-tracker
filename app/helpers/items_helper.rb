c4fbd7a97be693d6dff181495a8426f333abca50
Yousuf <syousuf-local@dhcp80ff86b1.dynamic.uiowa.edu>
2015-11-02 12:06:22 -0600

    Working with Item Import / Export

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,13 @@
 module ItemsHelper
+  def listing_actions item
+    actions = link_to 'Details', item, class: "btn btn-info actions", role:"button"
+    actions << (link_to 'Edit', edit_item_path(item), class: "btn btn-success actions", role:"button")
+    actions << (link_to 'Destroy', item, method: :delete, data: { confirm: 'Are you sure?' }, class: "btn btn-warning actions", role:"button")
+    actions
+  end
+
+  def importing_actions item
+    actions = link_to "Create",
+    actions
+  end
 end
