c5fd17fb9f8b50c7b73a3d89042b05dcca9c8474
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2015-11-07 21:05:00 -0600

    Removed logtailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-Rails.application.configure do
-  config.middleware.use Rails::Rack::LogTailer
-end
+# Rails.application.configure do
+#   config.middleware.use Rails::Rack::LogTailer
+# end
