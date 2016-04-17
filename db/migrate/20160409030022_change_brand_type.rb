f2d17a64acd12207e634514d8746404c7910ed2e
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2016-04-17 00:07:49 -0500

    Migration Fix
    * The Migration to change brand type to enum failed because of existing
      records. So added delete_all call

--- db/migrate/20160409030022_change_brand_type.rb
+++ db/migrate/20160409030022_change_brand_type.rb
@@ -1,5 +1,7 @@
 class ChangeBrandType < ActiveRecord::Migration
   def change
+    OrderItem.delete_all
+    Order.delete_all
     change_column :orders, :brand, 'integer USING CAST("brand" AS integer)'
     change_column :order_items, :brand, 'integer USING CAST("brand" AS integer)'
   end
