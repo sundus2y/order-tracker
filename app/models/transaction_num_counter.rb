380245cb3dd6a9e5065e6dc1aa032a661deb7f52
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2016-07-24 19:44:53 -0500

    Counter fix.
    * Made sure that the counter is reset when the date is changed.

--- app/models/transaction_num_counter.rb
+++ app/models/transaction_num_counter.rb
@@ -3,8 +3,9 @@ class TransactionNumCounter < ActiveRecord::Base
   belongs_to :store
 
   def self.get_transaction_next_num_for(store_id)
-    current = where(store_id: store_id).where("created_at <=  '#{DateTime.now.strftime('%Y-%m-%d')}'::date + '1 day'::interval").first
+    current = where(store_id: 4).where("created_at >= ? and created_at <= ?",Time.zone.now.beginning_of_day,Time.zone.now.end_of_day).first
     unless current
+      where(store_id: store_id).delete_all
       current = create(store_id: store_id, counter: 0)
     end
     current.counter += 1
