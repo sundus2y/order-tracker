429bc498de6aff7bac45ddf38fe3eda5e01e14a2
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2017-11-06 22:58:43 -0600

    Refactor Excel Download

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -15,15 +15,7 @@ class Inventory < ActiveRecord::Base
     table_heading_format = workbook.add_format(bold: 1)
     worksheet.merge_range('A1:Q1','Inventory for All Stores', heading_format)
     worksheet.write(1,0,'No',table_heading_format)
-    worksheet.write(1,1,'Item Name',table_heading_format)
-    worksheet.write(1,2,'Item Number',table_heading_format)
-    worksheet.write(1,3,'Original Number',table_heading_format)
-    worksheet.write(1,4,'Brand',table_heading_format)
-    worksheet.write(1,5,'Made',table_heading_format)
-    worksheet.write(1,6,'Dubai Price', table_heading_format)
-    worksheet.write(1,7,'Korea Price', table_heading_format)
-    worksheet.write(1,8,'Cost Price', table_heading_format)
-    worksheet.write(1,9,'Sale Price', table_heading_format)
+    write_item_header(table_heading_format, worksheet)
     start_index = 10
     Store.minus_virtual.each_with_index do |store,index|
       worksheet.write(1,start_index+index,store.name.titleize.upcase,table_heading_format)
@@ -33,15 +25,7 @@ class Inventory < ActiveRecord::Base
     item_inv.each_with_index do |(key,invs),index|
       begin
         worksheet.write(index+2,0,index+1)
-        worksheet.write_string(index+2,1,invs[0].item.name)
-        worksheet.write_string(index+2,2,invs[0].item.item_number)
-        worksheet.write_string(index+2,3,invs[0].item.original_number)
-        worksheet.write_string(index+2,4,invs[0].item.brand)
-        worksheet.write_string(index+2,5,invs[0].item.made)
-        worksheet.write_number(index+2,6,invs[0].item.dubai_price)
-        worksheet.write_number(index+2,7,invs[0].item.korea_price)
-        worksheet.write_number(index+2,8,invs[0].item.cost_price)
-        worksheet.write_number(index+2,9,invs[0].item.sale_price)
+        write_item_cell(index+2, invs[0].item, worksheet)
         invs.each_with_index do |inv,i|
           worksheet.write_number(index+2,store_column_map[inv.store_id],inv.qty) if store_column_map[inv.store_id]
         end
@@ -60,35 +44,46 @@ class Inventory < ActiveRecord::Base
     table_heading_format = workbook.add_format(bold: 1)
     worksheet.merge_range('A1:K1', "Inventory for #{store.name}", heading_format)
     worksheet.write(1,0,'No',table_heading_format)
-    worksheet.write_string(1,1,'Item Name',table_heading_format)
-    worksheet.write_string(1,2,'Item Number',table_heading_format)
-    worksheet.write_string(1,3,'Original Number',table_heading_format)
-    worksheet.write_string(1,4,'Brand',table_heading_format)
-    worksheet.write_string(1,5,'Made',table_heading_format)
-    worksheet.write(1,6,'Dubai Price', table_heading_format)
-    worksheet.write(1,7,'Korea Price', table_heading_format)
-    worksheet.write(1,8,'Cost Price', table_heading_format)
-    worksheet.write(1,9,'Sale Price', table_heading_format)
+    write_item_header(table_heading_format, worksheet)
     worksheet.write(1,10,'Qty',table_heading_format)
     item_inv = includes(:item).where(store: store)
     item_inv.each_with_index do |inv, index|
       begin
         worksheet.write(index+2,0, index+1)
-        worksheet.write(index+2,1, inv.item.name)
-        worksheet.write_string(index+2,2, inv.item.item_number)
-        worksheet.write_string(index+2,3, inv.item.original_number)
-        worksheet.write_string(index+2,4, inv.item.brand)
-        worksheet.write_string(index+2,5, inv.item.made)
-        worksheet.write_number(index+2,6, inv.item.dubai_price)
-        worksheet.write_number(index+2,7, inv.item.korea_price)
-        worksheet.write_number(index+2,8, inv.item.cost_price)
-        worksheet.write_number(index+2,9, inv.item.sale_price)
+        write_item_cell(index+2, inv.item, worksheet)
         worksheet.write_number(index+2,10, inv.qty)
       rescue Exception => e
       end
     end
     workbook.close
     File.open("tmp/#{store.name} Inventory.xlsx").path
+  end
+
+
+private
+
+  def self.write_item_header(table_heading_format, worksheet)
+    worksheet.write(1, 1, 'Item Name', table_heading_format)
+    worksheet.write(1, 2, 'Item Number', table_heading_format)
+    worksheet.write(1, 3, 'Original Number', table_heading_format)
+    worksheet.write(1, 4, 'Brand', table_heading_format)
+    worksheet.write(1, 5, 'Made', table_heading_format)
+    worksheet.write(1, 6, 'Dubai Price', table_heading_format)
+    worksheet.write(1, 7, 'Korea Price', table_heading_format)
+    worksheet.write(1, 8, 'Cost Price', table_heading_format)
+    worksheet.write(1, 9, 'Sale Price', table_heading_format)
+  end
+
+  def self.write_item_cell(row, item, worksheet)
+    worksheet.write_string(row, 1, item.name)
+    worksheet.write_string(row, 2, item.item_number)
+    worksheet.write_string(row, 3, item.original_number)
+    worksheet.write_string(row, 4, item.brand)
+    worksheet.write_string(row, 5, item.made)
+    worksheet.write_number(row, 6, item.dubai_price)
+    worksheet.write_number(row, 7, item.korea_price)
+    worksheet.write_number(row, 8, item.cost_price)
+    worksheet.write_number(row, 9, item.sale_price)
   end
 
 end
