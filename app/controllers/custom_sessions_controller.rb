395935dd03720eb44e0f1cef205c84f60be76501
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2018-01-08 10:54:26 -0600

    Fix off_hours logic

--- app/controllers/custom_sessions_controller.rb
+++ app/controllers/custom_sessions_controller.rb
@@ -18,7 +18,8 @@ class CustomSessionsController < Devise::SessionsController
     # Is Sunday?
     return true if login_time.sunday?
     # Is Between 6:30PM and 7AM?
-    return true if login_time.hour < 4 || login_time.hour > 16 || (login_time.hour == 15 && login_time.min > 30)
+    debugger
+    return true if login_time.hour < 4 || login_time.hour >= 16 || (login_time.hour == 15 && login_time.min > 30)
     false
   end
 end
