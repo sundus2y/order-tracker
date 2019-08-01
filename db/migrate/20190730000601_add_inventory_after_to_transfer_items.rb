3ea2e594d1ddcd33fcd0f3d4e74d9af3745b16f6
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2019-07-31 22:14:05 -0500

    Fix rails 4 migration for rails5

--- db/migrate/20190730000601_add_inventory_after_to_transfer_items.rb
+++ db/migrate/20190730000601_add_inventory_after_to_transfer_items.rb
@@ -1,4 +1,4 @@
-class AddInventoryAfterToTransferItems < ActiveRecord::Migration
+class AddInventoryAfterToTransferItems <  ActiveRecord::Migration[4.2]
   def change
     add_column :transfer_items, :inventory_after, :integer
   end
